d3d7d9ab1e37117d2ee3941af95c46aa917c5120
japrogramer <japrogramer@yahoo.com>
2014-04-13 03:47:03 -0500

    some to_python and db_type logic

--- uuid_contour/fields.py
+++ uuid_contour/fields.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils.translation import ugettext as _
 
+import uuid
+import re
+
 class UUIDContour(models.Field, metaclass=models.SubfieldBase):
     description = _('uuid(%(standard)s) max_length is %(max_length)s')
 
@@ -8,9 +11,21 @@ class UUIDContour(models.Field, metaclass=models.SubfieldBase):
         kwargs['max_length'] = 36
         super(UUIDContour, self).__init__(*args, **kwargs)
 
+    def db_type(self, connection):
+        if connection.settings_dict['ENGINE'] ==
+            'django.db.backends.postgresql_psycopg2':
+            return 'uuid'
+        return 'char(%s)' % self.max_length
+
     def to_python(self, value):
         if not value:
             return None
+        if isinstance(value, ):
+            return value
+        # uuids, xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx where M is the
+        # version and N is the variant
+        pu = re.compile('[a-f0-9]{8}-[a-f0-9]{4}-(?P<version>[1-5])[a-f0-9]{3}'
+                '-(?P<variant>[89ab])[a-f0-9]{3}-[a-f0-9]{12}')
         return super
 
     def formfield(self):
